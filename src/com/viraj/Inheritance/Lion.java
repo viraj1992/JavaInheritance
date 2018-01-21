8cd0153d07fc1fd379b643a38dcbb35a3e24963a
Viraj Pai <coder.viraj@gmail.com>
2018-01-21 22:11:32 +0530

    Misc Changes

--- src/com/viraj/Inheritance/Lion.java
+++ src/com/viraj/Inheritance/Lion.java
@@ -1,5 +1,11 @@
 package com.viraj.Inheritance;
 
-public class Lion {
+public class Lion extends Cat {
+
+	private boolean isBrave;
+
+	public Lion(String name, String color, int amountOfSpeed, int amountOfPower, int numberOflegs, boolean canHuntOtherAnimals) {
+		super(name, color, amountOfSpeed, amountOfPower, numberOflegs, canHuntOtherAnimals);
+	}
 
 }
