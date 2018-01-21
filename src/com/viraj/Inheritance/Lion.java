eba7620f1e81adfb4b517910a162cd30a36558d4
Viraj Pai <coder.viraj@gmail.com>
2018-01-21 22:15:45 +0530

    Misc Changes

--- src/com/viraj/Inheritance/Lion.java
+++ src/com/viraj/Inheritance/Lion.java
@@ -4,8 +4,9 @@ public class Lion extends Cat {
 
 	private boolean isBrave;
 
-	public Lion(String name, String color, int amountOfSpeed, int amountOfPower, int numberOflegs, boolean canHuntOtherAnimals) {
+	public Lion(String name, String color, int amountOfSpeed, int amountOfPower, int numberOflegs, boolean canHuntOtherAnimals, boolean isBrave) {
 		super(name, color, amountOfSpeed, amountOfPower, numberOflegs, canHuntOtherAnimals);
+		this.isBrave = isBrave;
 	}
 
 }
